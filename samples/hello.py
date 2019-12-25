606a025663b4bf969081ccfc317684939b8b72d5
LeeHemingway <coherence0815@gmail.com>
2019-12-26 00:02:00 +0900

    change name

---
+++ samples/hello.py
@@ -0,0 +1,25 @@
+import numpy as np
+import retro
+
+def main():
+    print(retro.data.list_games())
+
+    env = retro.make(game='SpaceInvaders-Atari2600')
+    print(f"The size of our frame is: {env.observation_space}")
+    print(f"The action size is : {env.action_space.n}")
+
+    possible_actions = np.array(np.identity(env.action_space.n,dtype=int).tolist())
+    print(f"possible actions:\n{possible_actions}")
+
+    obs = env.reset()
+    while True:
+        obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            obs = env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
+
